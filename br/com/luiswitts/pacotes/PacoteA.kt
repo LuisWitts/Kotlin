e2f2ff690311c058994b3094cb54f1130120abd6
LuisWitts <luis.witts@outlook.com>
2021-04-15 16:20:49 -0300

    Estrutura de Diretórios e Pacotes

---
+++ br/com/luiswitts/pacotes/PacoteA.kt
@@ -0,0 +1,4 @@
+package br.com.luiswitts.pacotes
+
+fun nome() = "Pacote A"
+
---
+++ br/com/luiswitts/pacotes/PacoteB.kt
@@ -0,0 +1,9 @@
+package br.com.luiswitts.pacotes
+
+//import br.com.luiswitts.pacotes.nome
+
+
+
+fun main(){
+    println(nome())
+}
