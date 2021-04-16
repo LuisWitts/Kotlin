ab0590fc4472dfb84b3eccb539667dc415e9d1c7
LuisWitts <luis.witts@outlook.com>
2021-04-16 19:49:03 -0300

    Resolução URI1002

--- URI1002.kt
+++ URI1002.kt
@@ -1,13 +1,28 @@
 fun main(args:Array<String>){
+
+    //Variaveis
     val pi:Double
     pi = 3.14159
     val area:Double
-    var raio:Double = readLine()!!.toDouble()
     var raio2:Double
+
+    //Entrada de dados
+    var raio:Double = readLine()!!.toDouble()
+
+    //Processamento
     raio2 = raio * raio
     area = raio2 * pi
     val area2: String = "%.4f".format(area)
+
+    //Saida de dados
+    
     println("A=${area2}")
+
+
+
+
+
+
 }
 
 
