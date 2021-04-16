1a26e08c9cc6bb3482fcb4aead2b7b37de34f0c4
LuisWitts <luis.witts@outlook.com>
2021-04-16 20:33:11 -0300

    Resolução URI1003

---
+++ URI1003.kt
@@ -0,0 +1,15 @@
+fun main (args: Array<String>){
+    //entrada de dados
+    var a:Int = readLine()!!.toInt()
+    var b:Int = readLine()!!.toInt()
+    //println("$a $b")
+
+    //processamento de dados
+    var soma:Int
+    soma = a + b
+
+    //saida de dados
+    println("SOMA = $soma")
+
+}
+
