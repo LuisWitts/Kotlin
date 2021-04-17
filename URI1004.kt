21b44d44e7c7455dc035db477f5ba8dfb5b13c9e
LuisWitts <luis.witts@outlook.com>
2021-04-16 23:39:07 -0300

    Resolução URI1004

---
+++ URI1004.kt
@@ -0,0 +1,12 @@
+fun main(args:Array<String>){
+    //entrada de dados
+    var n1:Int = readLine()!!.toInt()
+    var n2:Int = readLine()!!.toInt()
+    //println("$n1, $n2")
+
+    //processamento de dados
+    var prod =  n1 * n2
+
+    //saida de dados
+    println("PROD = $prod")
+}
